88f642c4c66fb6f863d0e2c08b5e4ac8328f493c
dharani sankar <veejayadeesh060993@gmail.com>
2026-05-28 14:57:19 +0530

    customer apis

--- src/controller/customer.ts
+++ src/controller/customer.ts
@@ -2,18 +2,93 @@ import { Request, Response } from "express";
 import { db } from "@/db/db.js";
 
 export const getCustomers = async (req: Request, res: Response) => {
-	const customers = await db.customer.findMany();
-	return res.status(200).json(customers);
+	try {
+		const customers = await db.customer.findMany();
+		return res.status(200).json({ data: customers, error: null });
+	} catch (e) {
+		if (e instanceof Error) {
+			console.error("Error in retrieving customer data", e.message);
+			return res
+				.status(500)
+				.json({ data: null, error: "Internal server error" });
+		}
+		console.error("Unknown error", e);
+		return res.status(500).json({ data: null, error: "Internal server error" });
+	}
 };
 
 export const createCustomer = async (req: Request, res: Response) => {
 	try {
-		const { customer } = req.body;
-		const result = await db.customer.create({ data: { ...customer } });
+		const {
+			customerType,
+			firstname,
+			lastname,
+			phone,
+			gender,
+			maxCreditLimit,
+			maxCreditDays,
+			country,
+			location,
+			taxPin,
+			dob,
+			email,
+			NIN,
+		} = req.body;
+		const result = await db.customer.create({
+			data: {
+				customerType,
+				firstname,
+				lastname,
+				phone,
+				gender,
+				maxCreditLimit,
+				maxCreditDays,
+				country,
+				location,
+				taxPin,
+				dob,
+				email,
+				NIN,
+			},
+		});
 		console.log("Created successfully");
-		return res.status(201).json(result);
+		return res.status(201).json({ data: result, error: null });
 	} catch (e) {
-		console.error(e);
+		if (e instanceof Error) {
+			console.error("Error in creating customer ", e.message);
+			if (
+				e.message.includes(
+					"Unique constraint failed on the constraint: `Customer_phone_key`",
+				)
+			) {
+				return res
+					.status(404)
+					.json({ data: null, error: "Phone number already in use" });
+			}
+			if (
+				e.message.includes(
+					"Unique constraint failed on the constraint: `Customer_email_key`",
+				)
+			) {
+				return res
+					.status(404)
+					.json({ data: null, error: "Email already in use" });
+			}
+			if (
+				e.message.includes(
+					"Unique constraint failed on the constraint: `Customer_NIN_key`",
+				)
+			) {
+				return res
+					.status(404)
+					.json({ data: null, error: "NIN already in use" });
+			}
+			return res
+				.status(500)
+				.json({ data: null, error: "Internal server error" });
+		}
+		console.error("Unknown error", e);
+		return res.status(500).json({ data: null, error: "Internal server error" });
 	}
 };
 
@@ -25,9 +100,18 @@ export const getCustomerById = async (req: Request, res: Response) => {
 				id,
 			},
 		});
-		return res.status(200).json(result);
+		if (!result) {
+			return res.status(404).json({ data: null, error: "Customer not found" });
+		}
+		return res.status(200).json({ data: result, error: null });
 	} catch (e) {
-		console.error(e);
-		return res.status(500).json("something went wrong");
+		if (e instanceof Error) {
+			console.error("Error in retrieving customer data", e.message);
+			return res
+				.status(500)
+				.json({ data: null, error: "Internal server error" });
+		}
+		console.error("Unknown error", e);
+		return res.status(500).json({ data: null, error: "Internal server error" });
 	}
 };
---
+++ src/routes/supplier.ts
@@ -0,0 +1,5 @@
+import express from "express";
+
+const supplierRounter = express.Router();
+
+export default supplierRounter;
